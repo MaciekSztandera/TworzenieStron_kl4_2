c2faac5efc4e89479075149b0ca2299e305db601
MaciekSztandera <144917410+MaciekSztandera@users.noreply.github.com>
2024-09-30 14:42:29 +0200

    php :PP

---
+++ rozpoczecie_roku_szkolnego.php
@@ -0,0 +1,9 @@
+<DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<p>Rozpoczęcie tego roku szkolnego odbyło się w <?php echo mktime()?>
+    <?php echo date("z",strtotime("2024-09-01"))?></p>
+</body>
+</html>
